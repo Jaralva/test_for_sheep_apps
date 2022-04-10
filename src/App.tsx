e254ee86f631f395721fed7f4faabd2540542f54
Jaralva <zaingrazes@mail.ru>
2022-04-10 19:20:50 +0300

    test0.3

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ import axios from "axios";
 function App() {
 
     const [rates , setrates] = useState('')
+    const [currency1, setCurrency1] = useState('USD')
+    const [currency2, setCurrency2] = useState('EUR')
+    const [value1,  setValue1] = useState(1)
+    const [value2,  setValue2] = useState(1)
 
     useEffect( ()=> {
         axios.get('http://data.fixer.io/api/latest?access_key=6730edfba906e693b4a62b73487efa34')
@@ -19,13 +23,27 @@ function App() {
             })
     }, [])
 
+    const currency1ChangeHandler = (currency: string) => {
+        setCurrency1(currency)
+    }
+    const currency2ChangeHandler = (currency: string) => {
+        setCurrency2(currency)
+    }
+
+
   return (
     <div className={s.App}>
         <div>
       <h2 className={s.title}> Exchange money </h2>
         <div className={s.windowsContainer}>
-          <ExchangeWindow rates={Object.keys(rates)}/>
-          <ExchangeWindow rates={Object.keys(rates)}/>
+          <ExchangeWindow
+              rates={Object.keys(rates)}
+              currency={currency1}
+              onCurrencyChangeHandler={currency1ChangeHandler}/>
+          <ExchangeWindow
+              rates={Object.keys(rates)}
+              currency={currency2}
+              onCurrencyChangeHandler={currency2ChangeHandler}/>
         </div>
       <div>
           <div>
