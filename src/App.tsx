3a2147757c6cb4c332a6aec23ce30c1bdebe71dc
Jaralva <zaingrazes@mail.ru>
2022-04-10 21:51:36 +0300

    test1.0

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,69 @@ import axios from "axios";
 
 function App() {
 
-    const [rates , setrates] = useState('')
-    const [currency1, setCurrency1] = useState('USD')
-    const [currency2, setCurrency2] = useState('EUR')
-    const [value1,  setValue1] = useState(1)
-    const [value2,  setValue2] = useState(1)
+    const [rates , setRates] = useState<{[key:string]:number}>({})
+    const [currency1, setCurrency1] = useState<string>('USD')
+    const [currency2, setCurrency2] = useState<string>('EUR')
+    const [value1,  setValue1] = useState<number>(0)
+    const [value2,  setValue2] = useState<number>(0)
 
     useEffect( ()=> {
         axios.get('http://data.fixer.io/api/latest?access_key=6730edfba906e693b4a62b73487efa34')
             .then(res => {
-                setrates(res.data.rates)
+                setRates(res.data.rates)
             })
             .catch(err => {
                 console.log(err)
             })
     }, [])
 
+
+        useEffect(() => {
+            if (!!rates) {
+                    onValue1ChangeHandler(1);
+            }
+        }, [rates]);
+
+
+
+
+
+    const rounding = (num : number) => {
+        return Number(num.toFixed(4))
+    }
+
     const currency1ChangeHandler = (currency: string) => {
         setCurrency1(currency)
+        setValue2(rounding(value1 * rates[currency2] / rates[currency] ))
     }
+
+    const onValue1ChangeHandler = ( value: number) => {
+        if (!value){
+            setValue2(0)
+            setValue1(0)
+            return
+        }
+        setValue1(value)
+        setValue2(rounding(value * rates[currency2] / rates[currency1]))
+
+    }
+
+    const onValue2ChangeHandler = ( value: number) => {
+        if (!value){
+            setValue2(0)
+            setValue1(0)
+            return
+        }
+        setValue2(value)
+        setValue1(rounding(value * rates[currency1] / rates[currency2] ))
+    }
+
     const currency2ChangeHandler = (currency: string) => {
         setCurrency2(currency)
+        setValue2(rounding(value1 * rates[currency] / rates[currency1] ))
     }
+
+
 
 
   return (
@@ -39,11 +80,15 @@ function App() {
           <ExchangeWindow
               rates={Object.keys(rates)}
               currency={currency1}
-              onCurrencyChangeHandler={currency1ChangeHandler}/>
+              onCurrencyChangeHandler={currency1ChangeHandler}
+              value={value1}
+              onValueChangeHandler={onValue1ChangeHandler}/>
           <ExchangeWindow
               rates={Object.keys(rates)}
               currency={currency2}
-              onCurrencyChangeHandler={currency2ChangeHandler}/>
+              onCurrencyChangeHandler={currency2ChangeHandler}
+              value={value2}
+              onValueChangeHandler={onValue2ChangeHandler}/>
         </div>
       <div>
           <div>
