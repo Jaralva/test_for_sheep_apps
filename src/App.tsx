b93eea38b9a0f85533833cbb67a60beda94ad188
Jaralva <zaingrazes@mail.ru>
2022-04-10 17:54:49 +0300

    fixer_api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import s from'./App.module.css';
+import {ExchangeWindow} from "./components/window";
+import {getExchangeRate} from "./api/api";
+import axios from "axios";
 
 function App() {
+
+    const [rates , setrates] = useState('')
+
+    useEffect( ()=> {
+        axios.get('http://data.fixer.io/api/latest?access_key=6730edfba906e693b4a62b73487efa34')
+            .then(res => {
+                setrates(res.data.rates)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={s.App}>
+        <div>
+      <h2 className={s.title}> Exchange money </h2>
+        <div className={s.windowsContainer}>
+          <ExchangeWindow rates={rates}/>
+          <ExchangeWindow rates={rates}/>
+        </div>
+      <div>
+          <div>
+              <button></button>
+          </div>
+          <div>
+              <div>Current value:</div>
+              <div>"VALUE"</div>
+          </div>
+      </div>
+        </div>
     </div>
   );
 }
