318b8c04d600f99e046103451be68728fc645fea
Jaralva <zaingrazes@mail.ru>
2022-04-17 00:23:52 +0300

    test1.3.1

--- src/components/window.tsx
+++ src/components/window.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from "react";
+import React from "react";
 import s from './window.module.css';
 import FormControl from '@mui/material/FormControl';
 import NativeSelect from '@mui/material/NativeSelect';
